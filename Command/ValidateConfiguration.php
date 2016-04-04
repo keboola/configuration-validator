9225befff7b8766bbb1d391b46068f71e5a7f625
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2016-04-04 12:16:33 +0200

    validate single file

--- Command/ValidateConfiguration.php
+++ Command/ValidateConfiguration.php
@@ -2,10 +2,12 @@
 
 namespace Keboola\ConfigurationValidator\Command;
 
+use JsonSchema\Validator;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
 
 class ValidateConfiguration extends Command
 {
@@ -20,7 +22,7 @@ class ValidateConfiguration extends Command
                 'Path to the schema'
             )
             ->addArgument(
-               'configuration',
+               'config',
                null,
                 InputArgument::REQUIRED,
                'Path to the configuration JSON'
@@ -28,9 +30,48 @@ class ValidateConfiguration extends Command
         ;
     }
 
+    protected function error(OutputInterface $output, $message, $code=1) {
+        $output->writeln($message);
+        return $code;
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('test');
+        $fs = new Filesystem();
+
+        if (!$fs->exists($input->getArgument("schema"))) {
+            return $this->error($output, "Schema file does not exist.", 1);
+        }
+
+        if (!$fs->exists($input->getArgument("config"))) {
+            return $this->error($output, "Configuration file does not exist.", 1);
+        }
+
+        $schema = json_decode(file_get_contents($input->getArgument("schema")));
+        if (!$schema) {
+            return $this->error($output, "Schema file empty or invalid JSON.", 1);
+        }
+
+        $config = json_decode(file_get_contents($input->getArgument("config")));
+        if (!$config) {
+            return $this->error($output, "Configuration file empty or invalid JSON.", 1);
+        }
+
+        $validator = new Validator();
+        $validator->check($config, $schema);
+        if (!$validator->isValid()) {
+            $message = "";
+            foreach ($validator->getErrors() as $error) {
+                if ($error['property']) {
+                    $message .= $error['property'] . ' ' . $error['message'] . "\n";
+                } else {
+                    $message .= $error['message'];
+                }
+            }
+            return $this->error($output, $message, 1);
+        }
+        $output->writeln("JSON valid");
     }
+
 }
 
